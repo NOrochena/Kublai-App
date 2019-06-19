a08b7de247d08d4d57f1195167207644e2798e42
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-19 04:58:00 -0400

    Added single user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'the truth' do
+    assert true
+  end
+
+  test 'should not save user without password' do
+    user = User.new(name: 'Nick', email: 'nick@email.com')
+    assert_not user.save
+  end
 end
