8b1479359e606ccdf0f6b153e512a6d12778472b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-19 16:12:27 -0400

    Added validation to Users, Comments, Collaborates, Tags

--- app/models/collaborator.rb
+++ app/models/collaborator.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 class Collaborator < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  validates :post_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :post_id, message: 'Already Collaborating on this project.' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :posts, through: :comments
   has_many :posts
   has_many :collaborators
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 end
